c98bb9fb32c0128e2411913d506831bdc6db430e
Joshua Drubin <j.drubin94@gmail.com>
2014-08-03 02:35:27 -0400

    added userInfoFragment and user_info_layout, now a User object is passed to all fragments.

--- app/src/main/java/com/hartfordlab/www/treehood/ChallengeDetailsFragment.java
+++ app/src/main/java/com/hartfordlab/www/treehood/ChallengeDetailsFragment.java
@@ -6,23 +6,44 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class ChallengeDetailsFragment extends Fragment {
+    User user;
+    Challenge challenge;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
         Bundle b = getArguments();
-        Challenge challenge = (Challenge)b.getSerializable("CHALLENGE");
+        final Challenge challenge = (Challenge)b.getSerializable("CHALLENGE");
+
+        user = (User)b.getSerializable("USER");
+
         View myInflatedView = inflater.inflate(R.layout.challenge_details_layout,container,false);
         Typeface roboto = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Roboto-Bold.ttf");
 
         ((TextView)myInflatedView.findViewById(R.id.title)).setText(challenge.getName());
         ((TextView)myInflatedView.findViewById(R.id.description)).setText(challenge.getDescription());
+
+        Button button = (Button)myInflatedView.findViewById(R.id.accept_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Challenge clone = challenge.clone();
+                clone.setProgress(Challenge.IN_PROGRESS);
+
+                user.addChallenge(clone);
+
+                Toast.makeText(container.getContext(), "Challenge Added!", Toast.LENGTH_SHORT).show();
+
+                getActivity().getFragmentManager().popBackStack();            }
+        });
 
         return myInflatedView;
     }

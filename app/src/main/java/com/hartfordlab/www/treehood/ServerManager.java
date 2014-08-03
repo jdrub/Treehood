dffcef1a79d30df341f7a123d9e4596695ebebbb
Joshua Drubin <j.drubin94@gmail.com>
2014-08-03 01:33:59 -0400

    true (faked) user login info

--- app/src/main/java/com/hartfordlab/www/treehood/ServerManager.java
+++ app/src/main/java/com/hartfordlab/www/treehood/ServerManager.java
@@ -11,15 +11,16 @@ public class ServerManager {
         //TODO: open the csv file here
     }
 
-    public User login(String username,String password){
-        //TODO: pull user from a locally-stored queue, or return null if user doesn't exist
-        String treeName = "Mill Creek";
-        String address = "7405 Cliffbourne Ct.";
-        int score = 2;
-        ArrayList<Challenge> challenges = new ArrayList<Challenge>();
-        challenges.add(new Challenge("Recycling Challenge",Challenge.IN_PROGRESS,
-                "Collect 100 used/empty bottles!"));
-        return new User(treeName,username,password,address,score,challenges);
+    public User login(String username,String password,DataManager dataManager){
+
+        ArrayList<User> users = dataManager.getUsers();
+        for(User u: users){
+            if(u.getName().equals(username) && u.comparePassword(password)){
+                return u;
+            }
+        }
+
+        return null;
     }
 
 
